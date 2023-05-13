138a7bce93473307d17ebe14b0d696a9538b96a5
Icarus <sambeekman@hotmail.com>
2023-05-13 19:08:08 +1000

    added javascript features to prework, something to do with browser console

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+/*if (topic === "HTML") 
+    [console.log("Lets study HTML");]
+    else if (topic === "CSS")
+    [console.log("Lets study CSS");]
+    ......
+    else
+    [console.log("Try again loser")];
+
+*/
+
+for(var x = 0; x < topics.length; x++)
+[console.log(topics[x])];
